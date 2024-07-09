57d19216c0ea29164dd812d75dd68da7c1c34ebf
Humberto Rivero <humbertorivero9@gmail.com>
2024-07-09 13:13:50 -0300

    🌐 Chore tailwind.config.ts with new colors and font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,48 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"landing-image": "url('/img/landing-image.webp')",
+			},
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: "#000",
+			white: "#fff",
+			primary: {
+				DEFAULT: "#C1FD35",
+			},
+			secondary: {
+				DEFAULT: "#3A393E",
+			},
+			tertiary: {
+				DEFAULT: "#EEEAEA",
+			},
+			"black-primary": {
+				DEFAULT: "#201F22",
+			},
+		},
+		fontSize: {
+			base: ["1rem", "1.5rem"], // 16px
+			sm: ["0.875rem", "1.25rem"], // 14px
+			md: ["1.125rem", "1.75rem"], // 18px
+			lg: ["1.25rem", "1.75rem"], // 20px
+			xl: ["1.5rem", "2rem"], // 24px
+			"2xl": ["2rem", "2.5rem"], // 32px
+			"3xl": ["2.5rem", "3rem"], // 40px
+			"4xl": ["3rem", "3.5rem"], // 48px
+			"5xl": ["4rem", "4.5rem"], // 64px
+			"6xl": ["4.5rem", "5rem"], // 72px
+		},
+	},
+	plugins: [],
 };
 export default config;
