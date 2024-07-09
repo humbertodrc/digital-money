40f3453564556f1da84e679655cadf246cc18d64
Humberto Rivero <humbertorivero9@gmail.com>
2024-07-09 14:41:53 -0300

    📝  Add Footer component to RootLayout in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,34 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import {Open_Sans} from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/layout/footer/Footer";
 
 const openSans = Open_Sans({
-  weight: ['300', '400', '600', '800'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap'
-})
+	weight: ["300", "400", "600", "800"],
+	style: ["normal"],
+	subsets: ["latin"],
+	display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "Digital Money",
-  description: "A digital money app built with Next.js and Tailwind CSS",
-  keywords: ["Digital Money", "Next.js", "Tailwind CSS"],
+	title: "Digital Money",
+	description: "A digital money app built with Next.js and Tailwind CSS",
+	keywords: ["Digital Money", "Next.js", "Tailwind CSS"],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="es">
-      <body className={`w-screen min-h-screen flex flex-col bg-black-primary text-white ${openSans.className}`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="es">
+			<body
+				className={`w-screen min-h-screen flex flex-col bg-black-primary text-white ${openSans.className}`}
+			>
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
