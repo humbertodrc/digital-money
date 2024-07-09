d79fec80fc5f2689da0f5d129e882fb4c7f6025f
Humberto Rivero <humbertorivero9@gmail.com>
2024-07-09 13:17:45 -0300

    🌐 Chore font in layout.tsx and add header and main sections in page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {Open_Sans} from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({
+  weight: ['300', '400', '600', '800'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: "Digital Money",
   description: "A digital money app built with Next.js and Tailwind CSS",
+  keywords: ["Digital Money", "Next.js", "Tailwind CSS"],
 };
 
 export default function RootLayout({
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`w-screen min-h-screen flex flex-col bg-black-primary text-white ${openSans.className}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const year = new Date().getFullYear();
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Digital Money</h1>
-    </main>
-  );
+	return (
+		<>
+			<header className="p-5 flex flex-row justify-between bg-transparent md:px-20 xl:px-40">
+				<Image
+					src="/img/logo.svg"
+					alt="Digital Money House"
+					width={80}
+					height={0}
+					className="hover:cursor-pointer"
+				/>
+				<div className="flex flex-row gap-2">
+					<Link
+						href={"/login"}
+						className="py-2 px-3 border-2 border-primary rounded-lg font-semibold text-center"
+					>
+						Ingresar
+					</Link>
+					<Link
+						href={"/signup"}
+						className="py-2 px-3 bg-primary rounded-lg text-black text-center font-semibold"
+					>
+						Crear cuenta
+					</Link>
+				</div>
+			</header>
+			<main className="relative w-full p-5 flex flex-col grow justify-between bg-landing-image bg-cover bg-center bg-no-repeat 2xl:bg-[50%_25%] md:p-20 xl:px-40">
+        <section className="w-2/3 pt-10 mb-10 flex flex-col gap-5 md:w-1/2 2xl:w-2/5">
+          <h1 className="font-semibold text-2xl md:text-4xl xl:text-5xl">De ahora en adelante, haces más con tu dinero</h1>
+          <div className="w-1/5 border-t-4 border-primary md:w-1/4 md:hidden"></div>
+          <h2 className="font-semibold text-xl text-primary md:text-2xl xl:text-3xl">Tu nueva <strong>billetera virtual</strong></h2>
+        </section>
+        <section className="flex flex-col gap-5 self-center z-20 md:w-2/3 xl:flex-row">
+          <div className="w-full p-5 bg-white rounded-xl text-black flex flex-col gap-2 md:p-7 md:gap-5">
+            <h3 className="pb-2 font-semibold text-xl border-b-2 border-primary md:text-2xl xl:text-3xl">Transferí dinero</h3>
+            <p className="text-sm md:text-base">Desde Digital Money House vas a poder transferir dinero a otras cuentas, asi como también recibir
+              transferencias y nuclear tu capital en nuestra billetera virtual.</p>
+          </div>
+          <div className="w-full p-5 bg-white rounded-xl text-black flex flex-col gap-2 md:p-7 md:gap-5">
+            <h3 className="pb-2 font-semibold text-xl border-b-2 border-primary md:text-2xl xl:text-3xl">Pago de servicios</h3>
+            <p className="text-sm md:text-base">Pagá mensualmente los servicios en 3 simples clicks. Facil, rápido y conveniente. Olvidate de las facturas en papel.</p>
+          </div>
+        </section>
+        <div className="absolute bottom-0 left-0 w-full h-2/5 rounded-t-2xl bg-primary z-10 md:h-1/3 xl:h-1/4"></div>
+      </main>
+      <footer className="p-5 bg-secondary text-primary text-center">
+			<p>© {year} Digital Money House</p>
+		</footer>
+		</>
+	);
 }
