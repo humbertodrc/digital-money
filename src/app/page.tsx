e7291c4b55b3bc439cdaaa28c345fac6dc60c203
Humberto Rivero <humbertorivero9@gmail.com>
2024-07-09 17:52:24 -0300

    📝 Update landing page with dynamic data from CMS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/landing/card/Card";
 import Title from "@/components/landing/title/Title";
 import Header from "@/components/layout/header/Header";
+import { landingData } from "@/data/landing";
 
 const links = [
 	{href: "/login", text: "Ingresar", outline: true},
@@ -11,21 +12,21 @@ export default function Home() {
 	return (
 		<>
 			<Header logoLink="/" logoClassName="fill-primary" links={links} />
-			<main className="relative w-full p-5 flex flex-col grow justify-between md:p-20 ">
+			<main className="relative w-full p-5 flex flex-col grow justify-between md:p-20 bg-landing-image bg-cover bg-center bg-no-repeat 2xl:bg-[50%_25%]">
 				<section className="w-2/3 pt-10 mb-10 flex flex-col gap-5 md:w-1/2 2xl:w-2/5">
 					<Title
-						title="De ahora en adelante, hacés más con tu dinero"
-						subtitle="Tu nueva <strong>billetera virtual</strong>"
+						title={landingData.title}
+						subtitle={landingData.subTitle}
 					/>
 				</section>
 				<section className="flex flex-col gap-5 self-center z-20 xl:flex-row">
 					<Card
-						title="Transferí dinero"
-						description="Desde Digital Money House vas a poder transferir dinero a otras cuentas, asi como también recibir transferencias y nuclear tu capital en nuestra billetera virtual."
+						title={landingData.cards.transfer.title}
+						description={landingData.cards.transfer.description}
 					/>
 					<Card
-						title="Pago de servicios"
-						description="Pagá mensualmente los servicios en 3 simples clicks. Facil, rápido y conveniente. Olvidate de las facturas en papel."
+						title={landingData.cards.services.title}
+						description={landingData.cards.services.description}
 					/>
 				</section>
 				<div className="absolute bottom-0 left-0 w-full h-2/5 rounded-t-2xl bg-primary z-10 md:h-1/3 xl:h-1/4"></div>
---
+++ src/data/landing.ts
@@ -0,0 +1,17 @@
+// TODO: en un futuro esta data puede venir de un CMS o de una API
+export const landingData = {
+	title: "De ahora en adelante, haces más con tu dinero",
+	subTitle: "Tu nueva <strong>billetera virtual</strong>",
+	cards: {
+		transfer: {
+			title: "Transferí dinero",
+			description:
+				"Desde Digital Money House vas a poder transferir dinero a otras cuentas, asi como también recibir transferencias y nuclear tu capital en nuestra billetera virtual.",
+		},
+		services: {
+			title: "Pago de servicios",
+			description:
+				"Pagá mensualmente los servicios en 3 simples clicks. Facil, rápido y conveniente. Olvidate de las facturas en papel.",
+		},
+	},
+};
