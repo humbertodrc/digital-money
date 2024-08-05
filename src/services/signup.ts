c7a4f348f859f9ab7168d3e5a0c0790ea85e5824
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-05 20:46:06 -0300

    🏗️ Refactor signup service to use interfaces and improve form validation

--- src/services/signup.ts
+++ src/services/signup.ts
@@ -1,20 +1,5 @@
+import {PostSignupBody, PostSignupResponse} from "@/interfaces/signup";
 import {httpPost} from "./common/http";
-
-interface PostSignupBody {
-	dni: number,
-  email: string,
-  firstname: string,
-  lastname: string,
-  password: string,
-  confirmPassword: string,
-  phone: string,
-}
-
-interface PostSignupResponse {
-	account_id: number;
-	email: string;
-	user_id: number;
-}
 
 export async function postSignup(
 	body: PostSignupBody,
