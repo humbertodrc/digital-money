3e13e4cfc309b5e883ee19e664df8a039e93e611
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-12 17:08:19 -0300

    🏗️ Refactor Step2Form component to improve password input styling and add form title

--- src/components/auth/loginForm/Step2Form.tsx
+++ src/components/auth/loginForm/Step2Form.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Button from "@/components/common/Button/Button";
 import TextInput from "@/components/common/TextInput";
-import { shemaPassword } from "@/schema/login";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Controller, useForm } from "react-hook-form";
+import {shemaPassword} from "@/schema/login";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {Controller, useForm} from "react-hook-form";
 
 interface Step2FormProps {
 	onSubmit: (data: any) => void;
@@ -13,7 +13,7 @@ export default function Step2Form({onSubmit}: Step2FormProps) {
 	const {
 		control,
 		handleSubmit,
-		formState: {errors},
+		formState: {errors, isSubmitting},
 	} = useForm({
 		resolver: yupResolver(shemaPassword),
 		defaultValues: {
@@ -44,9 +44,35 @@ export default function Step2Form({onSubmit}: Step2FormProps) {
 				/>
 				<Button
 					type="submit"
-					className="bg-primary hover:bg-primary-dark focus:outline-2 focus:outline-primary"
+					className="bg-primary hover:bg-primary-dark focus:outline-2 focus:outline-primary flex items-center justify-center"
 				>
-					Continuar
+					{isSubmitting ? (
+						<>
+							<svg
+								className="animate-spin -ml-1 mr-3 h-8 w-8 text-white"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									className="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									strokeWidth="4"
+								></circle>
+								<path
+									className="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								></path>
+							</svg>
+							<span className="ml-2">Enviando</span>
+						</>
+					) : (
+						"Continuar"
+					)}
 				</Button>
 			</form>
 		</>
