bc2d6fffe6744245d3229c8a264157f71d612b7b
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-12 15:16:48 -0300

    🏗️ Refactor LoginForm component to simplify code structure and improve user experience

--- src/components/auth/loginForm/LoginForm.tsx
+++ src/components/auth/loginForm/LoginForm.tsx
@@ -6,13 +6,11 @@ import Button from "@/components/common/Button/Button";
 import {LoginFormProps, UserData} from "@/interfaces/login";
 
 export default function LoginForm({isSignupSuccess}: LoginFormProps) {
-	const [step, setStep] = useState(1);
 	const [userData, setUserData] = useState<UserData>({email: ""});
 
 	// Manejador para el envío del primer paso (email)
 	const handleEmailSubmit = (data: {email: string}) => {
 		setUserData((prevData) => ({...prevData, email: data.email}));
-		setStep(2);
 	};
 
 	// Manejador para el envío del segundo paso (contraseña)
@@ -21,39 +19,25 @@ export default function LoginForm({isSignupSuccess}: LoginFormProps) {
 
 		// TODO: Implementar la llamada a la API
 		// const resp = await postLogin(updatedData);
-
 	};
-
-	// Si el usuario regresa al paso 1, por medio del browser, reseteamos el estado
-	if (step === 1 && userData.password) {
-		setUserData({email: ""});
-	}
 
 	return (
 		<main className="w-full px-4 md:px-0 flex flex-col items-center justify-center md:w-2/3 xl:w-96 mx-auto my-8">
-			{step === 1 && (
-				<>
-					<p className="text-lg font-semibold mb-10">
-						¡Hola! Ingresá tu e-mail
-					</p>
-					<Step1Form onSubmit={handleEmailSubmit} />
-					{!isSignupSuccess && (
-						<Button
-							asLink
-							href="/signup"
-							className="text-black bg-gray hover:bg-tertiary mt-8"
-						>
-							Crear cuenta
-						</Button>
-					)}
-				</>
-			)}
-			{step === 2 && (
-				<>
-					<p className="text-lg font-semibold mb-10">Ingresá tu contraseña</p>
-					<Step2Form onSubmit={handlePasswordSubmit} />
-				</>
-			)}
+			<div>
+				<Step1Form onSubmit={handleEmailSubmit} />
+				{!isSignupSuccess && (
+					<Button
+						asLink
+						href="/signup"
+						className="text-black bg-gray hover:bg-tertiary mt-8"
+					>
+						Crear cuenta
+					</Button>
+				)}
+			</div>
+			<div>
+				<Step2Form onSubmit={handlePasswordSubmit} />
+			</div>
 		</main>
 	);
 }
