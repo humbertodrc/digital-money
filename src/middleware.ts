144768fbe98eac4d5c32ace6120485f7d6e68249
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-14 14:49:00 -0300

    🛠️ Refactor middleware to handle signup success and redirect if necessary

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,21 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const cookieHasAuthToken = request.cookies.has("authToken");
+  const signupSuccess = request.cookies.has("signupSuccess");
+  console.log("signupSuccess", signupSuccess);
+  console.log('url', request.nextUrl.pathname);
   const url = request.nextUrl.clone();
 
-  // Verificamos si es la página de éxito y si tiene la bandera de registro exitoso
-  if (url.pathname === "/signup/success") {
-		const signupSuccess = request.cookies.get("signupSuccess");
-		console.log(signupSuccess);
-
-    if (!signupSuccess) {
-      return NextResponse.redirect(new URL("/signup", request.url));
-    }
-  }
-
-  // Verificamos si el usuario tiene un token de autenticación
-  if (!cookieHasAuthToken) {
+  // Verificamos que se pueda acceder a /signup/success solo si se ha registrado un usuario y tenemos la cookie
+  if (!signupSuccess && url.pathname === "/signup/success") {
     return NextResponse.redirect(new URL("/", request.url));
   }
+  
+
+  // Verificamos si el usuario tiene un token de autenticación
+  // if (!cookieHasAuthToken && url.pathname.startsWith("/dashboard")) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
   // Permitir el acceso a la ruta solicitada
   return NextResponse.next();
