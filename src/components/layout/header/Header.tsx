d0a53ff7fc955389bd4121f4216d7835cfaabb9c
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-19 10:39:27 -0300

    🏗️ Refactor Header component to include user name in the header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -14,9 +14,11 @@ interface HeaderProps {
 	logoLink: string;
 	headerClassName?: string;
 	links?: Link[];
+	userName?: string;
 }
 
-export default function Header({logoClassName, logoLink, headerClassName, links}: HeaderProps) {
+export default function Header({ logoClassName, logoLink, headerClassName, links, userName }: HeaderProps) {
+	
 	return (
 		<header className={clsx('p-5 flex flex-row justify-between md:px-20 xl:px-5', headerClassName)}>
 			<HeaderLogo className={logoClassName} href={logoLink} />
@@ -37,6 +39,16 @@ export default function Header({logoClassName, logoLink, headerClassName, links}
 						{link.text}
 					</Link>
 				))}
+				{userName && (
+					<div className="flex flex-row items-center gap-2">
+						{/* Iniciales */}
+						<div className="flex items-center justify-center p-2 bg-primary rounded-xl">
+							<span className="text-lg font-bold text-secondary">{userName.split(" ").map((word) => word[0]).join("")}</span>
+						</div>
+						{/* Nombre */}
+						<p>Hola, {userName}</p>
+					</div>
+				)}
 			</div>
 		</header>
 	);
