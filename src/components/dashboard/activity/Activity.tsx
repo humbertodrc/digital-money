02d6a5cbcac7ac9782a48747329583c9c13ec102
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-19 17:10:46 -0300

    🧮 Feat add Activity component to DashboardPage

---
+++ src/components/dashboard/activity/Activity.tsx
@@ -0,0 +1,28 @@
+import { activityDictionary } from "@/constants/activityDictionary";
+import { ActivityList } from "@/interfaces/activity";
+import { formatDayOfWeek } from "@/utils/formatDayOfWeek";
+import Link from "next/link";
+
+export default function Activity({activityList}: ActivityList) {
+  return (
+    <section className="w-full p-5 flex flex-col gap-5 rounded-md bg-white text-black shadow-md md:p-10 xl:p-15">
+    <h2 className="pb-5 border-b border-secondary text-base font-bold">Tu actividad</h2>
+    <ul className="flex flex-col gap-5">
+        {activityList.map((activity) => (
+          <li key={activity.id} className="w-full pb-5 flex flex-row justify-between items-center border-b border-secondary">
+          <div className="flex flex-row gap-2">
+              <h3>{activityDictionary[activity.type as keyof typeof activityDictionary]}</h3>
+          </div>
+          <div className="flex flex-col items-end">
+            <span className="text-md">$ {activity.amount},00</span>
+            <span className="text-sm opacity-50">{formatDayOfWeek(activity.dated)}</span>
+          </div>
+        </li>
+        ))}
+    </ul>
+    <Link href="/dashboard/activity" className="flex flex-row justify-between">
+      <span className="font-semibold">Ver toda tu actividad</span>
+    </Link>
+  </section>
+  )
+}
