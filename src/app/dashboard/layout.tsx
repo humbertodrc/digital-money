b412984459fd79f8014792724f3a30624cbf444c
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-19 17:10:14 -0300

    🛠️ Refactor DashboardLayout component to remove hardcoded accountInfo and userInfo objects

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,10 +13,6 @@ export default async function DashboardLayout({
 	children: React.ReactNode;
 	}) {
 	
-	// TODO: Get account info from API
-	const accountInfo = {
-		available_amount: 1000,
-	}
 	// TODO: Get user info from API
 	const userInfo = {
     "id": 19,
