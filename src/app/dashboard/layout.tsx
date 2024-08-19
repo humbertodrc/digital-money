733ac004d0e76ada51aa68ceeb86f66ea9dcd0c3
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-19 15:43:44 -0300

    📝 Add account info and user info to DashboardLayout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,48 @@
+import AcountInfo from "@/components/dashboard/accountInfo/AcountInfo";
+import Navbar from "@/components/dashboard/navbar/Navbar";
+import Header from "@/components/layout/header/Header";
+
 export const metadata = {
 	title: "Dashboard",
 	description: "Digital Money Dashboard",
 };
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
-}) {
-	return <>{children}</>;
+	}) {
+	
+	// TODO: Get account info from API
+	const accountInfo = {
+		available_amount: 1000,
+	}
+	// TODO: Get user info from API
+	const userInfo = {
+    "id": 19,
+    "firstname": "Daniel",
+    "lastname": "Rivero",
+    "dni": 30303030,
+    "email": "correo@correo.com",
+    "phone": "11999999"
+	}
+
+	
+	return (
+		<>
+			<Header
+				logoLink="/"
+				logoClassName="fill-primary"
+				userName={`${userInfo.firstname} ${userInfo.lastname}`}
+			/>
+			<div className="w-full h-full flex flex-row grow">
+				<aside className="hidden w-1/3 md:block xl:w-1/4 bg-primary text-black-primary">
+					<Navbar />
+				</aside>
+				<main className="w-full p-5 flex flex-col grow gap-5 items-center bg-tertiary-light md:p-10 xl:px-20">
+					{children}
+				</main>
+			</div>
+		</>
+	);
 }
