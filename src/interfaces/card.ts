c0eb9839c958267a0a5c4b37912eda3b70db1a2b
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-20 17:19:39 -0300

    📝 Add Card interface for representing card information

---
+++ src/interfaces/card.ts
@@ -0,0 +1,17 @@
+// {
+//   id: 24,
+//   account_id: 15,
+//   number_id: 4242424242424242,
+//   first_last_name: "Humberto Rivero",
+//   cod: 123,
+//   expiration_date: "12/2028",
+// }
+
+interface Card {
+  id: number;
+  account_id: number;
+  number_id: number;
+  first_last_name: string;
+  cod: number;
+  expiration_date: string;
+}
