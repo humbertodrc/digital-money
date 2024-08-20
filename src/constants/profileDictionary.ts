d35541e0ab284090598d2159cfc90440e67637a5
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-20 10:02:36 -0300

    📝 Add profileDictionary constant for mapping profile field names to display names

---
+++ src/constants/profileDictionary.ts
@@ -0,0 +1,14 @@
+
+const EMAIL = 'email';
+const DNI = 'dni';
+const PHONE = 'phone';
+const NAME = 'name';
+const PASSWORD = 'Contraseña';
+
+export const profileDictionary = {
+  [EMAIL]: 'Email',
+  [NAME]: 'Nombre y Apellido',
+  [DNI]: 'CUIT',
+  [PHONE]: 'Teléfono',
+  [PASSWORD]: 'Contraseña'
+};
