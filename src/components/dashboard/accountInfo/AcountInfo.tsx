5f029b0f1cd6847c23815d220be34b8ffb6aa163
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:54:02 -0300

    🏗️ Refactor accountInfo component to fix broken link and improve code readability

--- src/components/dashboard/accountInfo/AcountInfo.tsx
+++ src/components/dashboard/accountInfo/AcountInfo.tsx
@@ -6,11 +6,12 @@ interface AcountInfoProps {
 }
 
 export default function AcountInfo({ amount }: AcountInfoProps) {
+  
   return (
     <section className="w-full p-5 flex flex-col gap-5 rounded-lg bg-black-primary shadow-md md:p-10 xl:pl-6 xl:pr-[60px] xl:pb-12 xl:pt-8">
       <div className="flex flex-row gap-5 self-end">
         <Link href="/dashboard/cards" className="border-b text-base">Ver tarjetas</Link>
-        <Link href="/dashboard/deposit/bank" className="border-b text-base">Ver CVU</Link>
+        <Link href="/dashboard/deposit" className="border-b text-base">Ver CVU</Link>
       </div>
       <div className="flex flex-col gap-5">
         <h2 className="text-base font-semibold">Dinero disponible</h2>
