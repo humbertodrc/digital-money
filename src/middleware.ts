24a9c36c7791aafa0d1d02f30e967141d20499e0
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:50:23 -0300

    📝  Add middleware for authentication and authorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,41 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import {NextResponse} from "next/server";
+import type {NextRequest} from "next/server";
 
 export function middleware(request: NextRequest) {
-  const cookieHasAuthToken = request.cookies.has("authToken");
-  const signupSuccess = request.cookies.has("signupSuccess");
-  console.log("signupSuccess", signupSuccess);
-  console.log('url', request.nextUrl.pathname);
-  const url = request.nextUrl.clone();
+	const cookieHasAuthToken = request.cookies.has("authToken");
+	const signupSuccess = request.cookies.has("signupSuccess");
+	const url = request.nextUrl.clone();
 
-  // Verificamos que se pueda acceder a /signup/success solo si se ha registrado un usuario y tenemos la cookie
-  if (!signupSuccess && url.pathname === "/signup/success") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+	// Verificamos que se pueda acceder a /signup/success solo si se ha registrado un usuario y tenemos la cookie
+	if (!signupSuccess && url.pathname === "/signup/success") {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
+	// Verificamos si el usuario tiene un token de autenticación para rutas protegidas
+	if (!cookieHasAuthToken && url.pathname.startsWith("/dashboard")) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
+  // Si el usuario tiene un token de autenticación, lo enviamos en el header de la solicitud
+	// if (cookieHasAuthToken) {
+	// 	const authToken = request.cookies.get("authToken")?.value;
+  //   const requestHeaders = new Headers(request.headers);
+	// 	if (authToken) {
+  //     requestHeaders.set("Authorization", `${authToken}`);
+  //     requestHeaders.keys();
+  //   }
+    
+	// 	return NextResponse.next({
+	// 		request: {
+	// 			headers: requestHeaders,
+	// 		},
+	// 	});
+  // }
   
-
-  // Verificamos si el usuario tiene un token de autenticación
-  if (!cookieHasAuthToken && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  // Permitir el acceso a la ruta solicitada
-  return NextResponse.next();
+	// Permitir el acceso a la ruta solicitada
+	return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/signup/success"],
+	matcher: ["/dashboard/:path*", "/signup/success"],
 };
