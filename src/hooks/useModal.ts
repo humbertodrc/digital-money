08b5cf55de201ad161bcd51ef3af1274a3c38459
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:55:35 -0300

    🏗️ Refactor useModal hook to handle modal functionality

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,14 @@
+import { useCallback, useState } from 'react'
+
+export default function useModal() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const open = useCallback(() => setIsOpen(true), [])
+  const close = useCallback(() => setIsOpen(false), [])
+
+  return {
+    isOpen,
+    open,
+    close
+  }
+}
