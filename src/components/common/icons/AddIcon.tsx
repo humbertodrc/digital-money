f1307a7616d83088fc65d5894efbde8df4545f1f
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:52:44 -0300

    🏗️ Refactor import statement for AddIcon component

--- src/components/common/icons/AddIcon.tsx
+++ src/components/common/icons/AddIcon.tsx
@@ -4,7 +4,7 @@ interface AddIconProps {
 
 export default function AddIcon({className}: AddIconProps) {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="none">
+		<svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="none" className={className}>
 			<g stroke="#C1FD35" strokeWidth="1.3">
 				<circle cx="17" cy="17" r="16.35" />
 				<path strokeLinejoin="round" d="M16.75 10v14.5M24 17H9.5" />
