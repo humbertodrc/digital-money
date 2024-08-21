78f6b2ef33e352c4e59d2f44ae3afe981b3c2fcc
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:56:58 -0300

    🏗️ Refactor: update getUserInfo function to support revalidation and add patchUserInfo function

--- src/services/userInfo.ts
+++ src/services/userInfo.ts
@@ -1,9 +1,24 @@
-import {httpGet} from "./common/http";
+import {httpGetRevalidate, httpPatch} from "./common/http";
 
-export async function getUserInfo(id: number, token: string, options = {}): Promise<any> {
-	return httpGet(`/users/${id}`, token, {
+export async function getUserInfo(id: number, token: string, revalidateTag = "", options = {}): Promise<any> {
+	return httpGetRevalidate(`/users/${id}`, token, revalidateTag, {
 		headers: {
       "Content-Type": "application/json",
+		},
+		...options,
+	})
+		.then((data) => data as any)
+		.catch((error) => {
+			console.log(error);
+			throw error;
+		});
+}
+
+
+export async function patchUserInfo(id: number, body: any, options = {}): Promise<any> {
+	return httpPatch(`/users/${id}`,body, {
+		headers: {
+			"Content-Type": "application/json",
 		},
 		...options,
 	})
