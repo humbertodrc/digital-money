d00de511a88b9ad385d57bb0738f23935b5b8238
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:56:14 -0300

    🏗️ Refactor activity service to use httpGet function for fetching account activity

---
+++ src/services/activity.ts
@@ -0,0 +1,15 @@
+import {httpGet} from "./common/http";
+
+export async function getActivity(id: number, token: string, options = {}): Promise<any> {
+	return httpGet(`/accounts/${id}/activity`, token, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		...options,
+	})
+		.then((data) => data as any)
+		.catch((error) => {
+			console.log(error);
+			throw error;
+		});
+}
