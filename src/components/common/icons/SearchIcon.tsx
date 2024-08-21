e3fbed6b4c8739c504140f011d6235e9f9da3409
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:52:54 -0300

    🏗️ Refactor import statements for SearchIcon component

--- src/components/common/icons/SearchIcon.tsx
+++ src/components/common/icons/SearchIcon.tsx
@@ -12,11 +12,11 @@ export default function SearchIcon({ className }: SearchIconProps) {
       xmlns="http://www.w3.org/2000/svg"
       className={className}
 		>
-			<circle cx="6.5" cy="6.5" r="5.75" stroke="#828282" stroke-width="1.5" />
+			<circle cx="6.5" cy="6.5" r="5.75" stroke="#828282" strokeWidth="1.5" />
 			<path
 				d="M10.1992 11L14.7195 16.1191"
 				stroke="#828282"
-				stroke-width="1.5"
+				strokeWidth="1.5"
 			/>
 		</svg>
 	);
