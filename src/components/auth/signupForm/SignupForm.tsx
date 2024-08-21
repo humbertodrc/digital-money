47c5e3127eb6374e02faac106462d459e9a5b2bf
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:52:25 -0300

    🏗️ Refactor import statements for Button and TextInput components

--- src/components/auth/signupForm/SignupForm.tsx
+++ src/components/auth/signupForm/SignupForm.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Button from "@/components/common/Button/Button";
-import TextInput from "@/components/common/TextInput";
+import Button from "@/components/common/button/Button";
+import TextInput from "@/components/common/textInput/TextInput";
 import { SignupData } from "@/interfaces/signup";
 import { shema } from "@/schema";
 import { postSignup } from "@/services/signup";
