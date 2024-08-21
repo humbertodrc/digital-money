438be90c90873281f7d234a7251f6fbd2fcdc000
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-21 16:51:34 -0300

    📝 Update ProfilePage to fetch account and profile information

--- src/app/dashboard/cards/page.tsx
+++ src/app/dashboard/cards/page.tsx
@@ -1,101 +1,16 @@
 import AddIcon from "@/components/common/Icons/AddIcon";
 import ArrowRight from "@/components/common/Icons/ArrowRight";
 import CardsList from "@/components/dashboard/cardsList/CardsList";
+import { getAcountInfo } from "@/services/acountInfo";
+import { getCards } from "@/services/cards";
+import { getTokenFromCookie } from "@/utils/getTokenFromCookie";
 import Link from "next/link";
 
-const cards = [
-	{
-		id: 24,
-		account_id: 15,
-		number_id: 4242424242424242,
-		first_last_name: "Humberto Rivero",
-		cod: 123,
-		expiration_date: "12/2028",
-	},
-	{
-		id: 25,
-		account_id: 15,
-		number_id: 5252525252525252,
-		first_last_name: "Humberto Rivero",
-		cod: 363,
-		expiration_date: "12/2028",
-	},
-	{
-		id: 26,
-		account_id: 15,
-		number_id: 5452374545457545,
-		first_last_name: "Humberto",
-		cod: 363,
-		expiration_date: "06/2028",
-	},
-	{
-		id: 27,
-		account_id: 15,
-		number_id: 4542454545454545,
-		first_last_name: "Humberto",
-		cod: 363,
-		expiration_date: "03/2026",
-	},
-	{
-		id: 28,
-		account_id: 15,
-		number_id: 2325452253544454,
-		first_last_name: "Humberto Rivero",
-		cod: 363,
-		expiration_date: "03/2025",
-	},
-	{
-		id: 29,
-		account_id: 15,
-		number_id: 4525262645452554,
-		first_last_name: "Humberto",
-		cod: 363,
-		expiration_date: "12/2027",
-	},
-	{
-		id: 31,
-		account_id: 15,
-		number_id: 4545233634645245,
-		first_last_name: "Humberto",
-		cod: 363,
-		expiration_date: "12/2029",
-	},
-	{
-		id: 32,
-		account_id: 15,
-		number_id: 5252235746548582,
-		first_last_name: "Humberto",
-		cod: 363,
-		expiration_date: "12/2026",
-	},
-	{
-		id: 33,
-		account_id: 15,
-		number_id: 5225445555553636,
-		first_last_name: "Humberto",
-		cod: 363,
-		expiration_date: "12/2029",
-	},
-	{
-		id: 95,
-		account_id: 15,
-		number_id: 2424585698247825,
-		first_last_name: "Humberto",
-		cod: 60,
-		expiration_date: "07/2030",
-	},
-	{
-		id: 96,
-		account_id: 15,
-		number_id: 4242364668978978,
-		first_last_name: "Humberto",
-		cod: 63,
-		expiration_date: "07/2032",
-	},
-];
-
-export default function CardsPage() {
-  console.log(cards.length);
+export default async function CardsPage() {
+	const token = getTokenFromCookie();
+	const accountInfo = await getAcountInfo(token);
+	const cards = await getCards(accountInfo.id, token);
+	console.log(cards);
 	return (
 		<>
 			<section
@@ -118,8 +33,8 @@ export default function CardsPage() {
 					</div>
 					<ArrowRight className="fill-primary" />
 				</Link>
-      </section>
-      <CardsList cardsList={cards} />
+			</section>
+			<CardsList cardsList={cards} userId={accountInfo.user_id} />
 		</>
 	);
 }
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,29 +1,19 @@
 import ArrowRight from "@/components/common/Icons/ArrowRight";
 import AccountAndAlias from "@/components/dashboard/accountAndAlias/AccountAndAlias";
 import ProfileInfo from "@/components/dashboard/profileInfo/ProfileInfo";
+import { getAcountInfo } from "@/services/acountInfo";
+import { getUserInfo } from "@/services/userInfo";
+import { getTokenFromCookie } from "@/utils/getTokenFromCookie";
 import Link from "next/link";
 
-export default function ProfilePage() {
-	const profileInfo = {
-		id: 19,
-		firstname: "Daniel",
-		lastname: "Rivero",
-		dni: 95794558,
-		email: "humberto.rivero@digitalhouse.com",
-		phone: "1149458678",
-	};
-
-	const accountInfo = {
-		id: 15,
-		user_id: 19,
-		cvu: "50257992909156335",
-		alias: "PLANTA.ASADO.ARBOL",
-		available_amount: 54700,
-	};
+export default async function ProfilePage() {
+	const token = getTokenFromCookie();
+	const accountInfo = await getAcountInfo(token);
+	const profileInfo = await getUserInfo(accountInfo.user_id, token, "user-info");
 
 	return (
 		<>
-			<ProfileInfo profileInfo={profileInfo} />
+			<ProfileInfo profileInfo={profileInfo} token={token}  />
 			<div className="w-full py-5 bg-primary rounded-md text-black font-bold shadow-md pl-8 pr-14 pt-11 pb-12 flex items-center justify-between">
 				<Link
 					href="/dashboard/cards"
