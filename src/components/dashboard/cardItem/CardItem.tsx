9ecc54dce53ba018cac8754153a4aa5b45515a2d
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-25 18:43:29 -0300

    📝 Add router refresh after deleting a card in CardItem component

--- src/components/dashboard/cardItem/CardItem.tsx
+++ src/components/dashboard/cardItem/CardItem.tsx
@@ -2,17 +2,20 @@
 import Bullet from "@/components/common/Icons/Bullet";
 import { Card } from "@/interfaces/card";
 import { deleteCard } from "@/services/cards";
+import { useRouter } from "next/navigation";
 
 interface CardItemProps {
 	card: Card;
 	userId: number;
 }
 
-export default function CardItem({card, userId}: CardItemProps) {
+export default function CardItem({ card, userId }: CardItemProps) {
+	const router = useRouter();
 	const lastNumbers = card.number_id.toString().slice(-4);
 
 	const handleDelete = async(id: number) => {
 		await deleteCard(userId, id);
+		router.refresh();
 	};
 
 	return (
