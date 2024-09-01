8a77b4ca210020d216c12e92c94be59678cab2d2
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-31 23:32:56 -0300

    📝 Update AddAmount component to use react-hook-form for form control

--- src/components/dashboard/addAmount/AddAmount.tsx
+++ src/components/dashboard/addAmount/AddAmount.tsx
@@ -1,35 +1,44 @@
 import Button from "@/components/common/button/Button";
+import TextInput from "@/components/common/textInput/TextInput";
+import {useFormContext, Controller} from "react-hook-form";
 
 interface AddAmountProps {
-	handleNextStep2: (amount: number) => void;
+	handleNextStep: () => void;
 }
 
-export default function AddAmount({ handleNextStep2 }: AddAmountProps) {
-  
-
+export default function AddAmount({handleNextStep}: AddAmountProps) {
+	const {control} = useFormContext();
 
 	return (
 		<>
-			<section className="w-full pt-10 pl-14 pr-11 pb-11 flex flex-col rounded-lg bg-secondary shadow-md">
+			<section className="w-full pt-10 pl-14 pr-11 pb-11 flex flex-col rounded-lg bg-black-primary shadow-md">
 				<h2 className="text-xl text-primary font-semibold">
 					¿Cuánto querés ingresar a la cuenta?
 				</h2>
-				<div className="w-[360px] p-5 flex flex-row gap-1 items-center rounded-lg bg-white text-black mt-11">
+				<div className="w-[360px] p-5 flex flex-row gap-2 items-center rounded-lg bg-white text-black mt-11">
 					<span className="text-md text-black/50">$</span>
-					<input
-						type="number"
-						placeholder="0.00"
-						className="w-full bg-transparent text-md text-black/50 font-normal"
+					<Controller
+						name="amount"
+						control={control}
+						render={({field}) => (
+							<TextInput
+								{...field}
+								type="number"
+								placeholder="0.00"
+								wrapperClassName="w-full"
+								className="bg-transparent text-md text-black/50 font-normal"
+							/>
+						)}
 					/>
-        </div>
-        <div className="mt-9 w-full flex justify-end">
-        <Button
-            onClick={() => handleNextStep2(250)}
+				</div>
+				<div className="mt-9 w-full flex justify-end">
+					<Button
+						onClick={handleNextStep}
 						className="bg-primary hover:bg-primary-dark focus:outline-2 focus:outline-primary w-full xl:w-60"
 					>
 						Continuar
 					</Button>
-        </div>
+				</div>
 			</section>
 		</>
 	);
