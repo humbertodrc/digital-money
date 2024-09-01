03ed679f8470101e1d3a04b4a0d6fc9fe67d495e
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-31 23:34:25 -0300

    🛠️ Refactor deposits service to handle posting deposits to specific accounts

---
+++ src/services/deposits.ts
@@ -0,0 +1,22 @@
+import { httpPost } from "./common/http";
+
+interface Deposit {
+  origin: string;
+  destination: string;
+  dated: string;
+  amount: number;
+}
+
+export async function postDeposits(id: number, body: Deposit, options = {}): Promise<Deposit> {
+	return httpPost(`/accounts/${id}/deposits`, body, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		...options,
+	})
+		.then((data) => data as any)
+		.catch((error) => {
+			console.log(error);
+			throw error;
+		});
+}
