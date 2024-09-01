bacc31c951e434e15a313c68daae5169aee6a66c
Humberto Rivero <humbertorivero9@gmail.com>
2024-08-31 23:31:30 -0300

    🏗️ Refactor DashboardLayout component to fetch and display user and account information

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,7 +17,9 @@ export default async function DashboardLayout({
 	}) {
 	const token = getTokenFromCookie();
 	const accountInfo = await getAcountInfo(token);
+	console.log(accountInfo);
 	const userInfo = await getUserInfo(accountInfo.user_id, token, "user-info");
+	console.log(userInfo);
 	
 	return (
 		<>
