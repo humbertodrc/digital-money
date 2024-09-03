4c64624682114669a9e2a0063ad8c07e07380872
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-03 13:06:01 -0300

    📝 Add font-extrabold class to "Cargar dinero" link in Navbar component

--- src/components/dashboard/navbar/Navbar.tsx
+++ src/components/dashboard/navbar/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
 			<Link
 				href="/dashboard/deposit"
 				className={clsx("text-base", {
-					"font-extrabold": pathname === navbarPaths.deposit,
+					"font-extrabold": pathname === navbarPaths.deposit || pathname === navbarPaths.depositCard || pathname === navbarPaths.depositBank,
 				})}
 			>
 				Cargar dinero
