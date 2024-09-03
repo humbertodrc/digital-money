3e4c818803062a88ba088575b078fe3235e110df
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-03 13:00:50 -0300

    📝 Update Activity component to use filtered and paginated activity list

--- src/components/dashboard/activity/Activity.tsx
+++ src/components/dashboard/activity/Activity.tsx
@@ -2,87 +2,138 @@
 import ArrowRight from "@/components/common/Icons/ArrowRight";
 import Bullet from "@/components/common/Icons/Bullet";
 import SearchIcon from "@/components/common/Icons/SearchIcon";
-import {activityDictionary} from "@/constants/activityDictionary";
-import {ActivityList} from "@/interfaces/activity";
-import {formatDayOfWeek} from "@/utils/formatDayOfWeek";
+import { activityDictionary } from "@/constants/activityDictionary";
+import { useFilteredActivity } from "@/hooks/useFilteredActivity";
+import { usePagination } from "@/hooks/usePagination";
+import { ActivityItem } from "@/interfaces/activity";
+import { formatDayOfWeek } from "@/utils/formatDayOfWeek";
+import clsx from "clsx";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
-import {ChangeEvent, KeyboardEvent, useState} from "react";
+import OptionsFilter from "../optionsFilter/OptionsFilter";
+import { FILTER_OPTIONS } from "@/constants/filterOptions";
 
-export default function Activity({activityList}: ActivityList) {
-	// Filtar por type
-	const [searchInput, setSearchInput] = useState<string>("");
-	const router = useRouter();
+interface ActivityProps {
+	activityList: ActivityItem[];
+	showMoreActivity?: boolean;
+	hasOptionsActivity?: boolean;
+	hasPagination?: boolean;
+}
 
-	const handleSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setSearchInput(e.target.value);
-	};
+const itemsPerPage = 5;
 
-	const handleSearchInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-		if (e.key === "Enter") {
-			searchInput.trim();
-			redirectToActivityPage();
-		}
-	};
-
-	// Llevar al usuario a la página de actividad con un query param de lo que busca
-	const redirectToActivityPage = () => {
-		if (searchInput.trim().length > 0) {
-			router.push(`/dashboard/activity?search=${searchInput}`);
-		}
-	};
+export default function Activity({
+	activityList,
+	showMoreActivity,
+	hasOptionsActivity,
+	hasPagination,
+}: ActivityProps) {
+  // Hook para filtrar la lista de actividades
+	const {
+		filteredActivityList,
+		searchInput,
+		selectedFilter,
+		handleSearchInputChange,
+		handleSearchInputKeyDown,
+		handleFilterChange,
+		clearFilters,
+  } = useFilteredActivity(activityList);
+  // Hook para la paginación
+	const {currentItems, currentPage, totalPages, paginate} = usePagination(
+		filteredActivityList,
+		itemsPerPage
+	);
 
 	return (
 		<div className="w-full">
-      <div className="bg-white w-full flex items-center border border-gray-300 rounded-lg p-5 relative shadow-md mb-5">
-        <SearchIcon />
-				<input
-					type="text"
-					placeholder="Buscar en tu actividad"
-					className="flex-grow pl-2 py-1 text-base border-none focus:border-none focus-visible:outline-none text-black"
-					value={searchInput}
-					onKeyDown={handleSearchInputKeyDown}
-					onChange={handleSearchInputChange}
-				/>
+			<div className="flex items-center mb-5 gap-10">
+				<div className="bg-white w-full flex items-center border border-gray-300 rounded-lg p-5 relative shadow-md">
+					<SearchIcon />
+					<input
+						type="text"
+						placeholder="Buscar en tu actividad"
+						className="flex-grow pl-2 py-1 text-base border-none focus:border-none focus-visible:outline-none text-black"
+						value={searchInput}
+						onKeyDown={handleSearchInputKeyDown}
+						onChange={handleSearchInputChange}
+					/>
+				</div>
+				{hasOptionsActivity && (
+					<OptionsFilter
+						options={FILTER_OPTIONS}
+						selectedFilter={selectedFilter}
+						handleFilterChange={handleFilterChange}
+						clearFilters={clearFilters}
+					/>
+				)}
 			</div>
+
 			<section className="w-full p-5 flex flex-col gap-5 rounded-md bg-white text-black shadow-md md:p-10 xl:p-15">
 				<h2 className="pb-5 border-b border-secondary text-base font-bold">
 					Tu actividad
 				</h2>
 				<ul className="flex flex-col gap-5">
-					{activityList.map((activity) => (
-						<li
-							key={activity.id}
-							className="w-full pb-5 flex flex-row justify-between items-center border-b border-secondary"
-						>
-							<div className="flex flex-row gap-4">
-								<Bullet className="fill-primary" />
-								<h3>
-									{
-										activityDictionary[
-											activity.type as keyof typeof activityDictionary
-										]
-									}
-								</h3>
-							</div>
-							<div className="flex flex-col items-end">
-								<span className="text-md">$ {activity.amount},00</span>
-								<span className="text-sm opacity-50">
-									{formatDayOfWeek(activity.dated)}
-								</span>
-							</div>
-						</li>
-					))}
+					{currentItems.length > 0 ? (
+						currentItems.map((activity) => (
+							<li
+								key={activity.id}
+								className="w-full pb-5 flex flex-row justify-between items-center border-b border-secondary"
+							>
+								<div className="flex flex-row gap-4">
+									<Bullet className="fill-primary" />
+									<h3>
+										{
+											activityDictionary[
+												activity.type as keyof typeof activityDictionary
+											]
+										}
+									</h3>
+								</div>
+								<div className="flex flex-col items-end">
+									<span className="text-md">$ {activity.amount},00</span>
+									<span className="text-sm opacity-50">
+										{formatDayOfWeek(activity.dated)}
+									</span>
+								</div>
+							</li>
+						))
+					) : (
+						<li>No se encontraron actividades</li>
+					)}
 				</ul>
-				<Link
-					href="/dashboard/activity"
-					className="flex flex-row justify-between"
-				>
-					<span className="text-base text-black font-bold">
-						Ver toda tu actividad
-					</span>
-					<ArrowRight className="fill-secondary" />
-				</Link>
+
+				{hasPagination && totalPages > 1 && (
+					<div className="flex justify-center mt-5">
+						{totalPages > 0 &&
+							Array.from({length: totalPages}, (_, index) => (
+								<button
+									key={index + 1}
+									className={clsx(
+										"px-4 py-2 mx-1 rounded-lg text-black text-base font-bold",
+										{
+											"bg-tertiary": index + 1 === currentPage,
+											"bg-transparent hover:bg-tertiary":
+												index + 1 !== currentPage,
+										}
+									)}
+									onClick={() => paginate(index + 1)}
+								>
+									{index + 1}
+								</button>
+							))}
+					</div>
+				)}
+
+				{showMoreActivity && (
+					<Link
+						href="/dashboard/activity"
+						className="flex flex-row justify-between mt-5"
+					>
+						<span className="text-base text-black font-bold">
+							Ver toda tu actividad
+						</span>
+						<ArrowRight className="fill-secondary" />
+					</Link>
+				)}
 			</section>
 		</div>
 	);
