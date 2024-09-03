bd66aa4089313eeedd784f9536065ac0bd3a24b8
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-03 13:06:44 -0300

    📝 Add filter options constant file

---
+++ src/constants/filterOptions.ts
@@ -0,0 +1,8 @@
+export const FILTER_OPTIONS = [
+	{label: "Hoy", value: "today"},
+	{label: "Ayer", value: "yesterday"},
+	{label: "Última semana", value: "lastWeek"},
+	{label: "Últimos 15 días", value: "last15Days"},
+	{label: "Último mes", value: "lastMonth"},
+	{label: "Último año", value: "lastYear"},
+];
