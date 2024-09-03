654f279b61e7a71ab7441c10d7af1d7a0dfe7d8f
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-03 13:00:14 -0300

    🏗️ Refactor ActivityPage component to fetch and display activities in reverse order

--- src/app/dashboard/activity/page.tsx
+++ src/app/dashboard/activity/page.tsx
@@ -1,8 +1,23 @@
+import Activity from "@/components/dashboard/activity/Activity";
+import { getAcountInfo } from "@/services/acountInfo";
+import { getActivity } from "@/services/activity";
+import { getTokenFromCookie } from "@/utils/getTokenFromCookie";
 
-export default function ActivityPage() {
+export default async function ActivityPage() {
+
+  const token = getTokenFromCookie();
+	const accountInfo = await getAcountInfo(token);
+  const activities = await getActivity(accountInfo.id, token);
+  
+  const activitiesShowByOrder = activities.toReversed();
+
   return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
+    <>
+      <Activity
+        activityList={activitiesShowByOrder}
+        hasOptionsActivity
+        hasPagination
+      />
+    </>
   );
 }
