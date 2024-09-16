c335e13e0a9c699b231f2344541d030a77d9bd68
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-16 20:02:08 -0300

    🏗️ Refactor PaymentPage component to use ContainerService instead of ServiceList

--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -1,14 +1,12 @@
-import ServiceList from "@/components/dashboard/serviceList/ServiceList";
+import ContainerService from "@/components/dashboard/containerService/ContainerService";
 import { getService } from "@/services/service";
 
 export default async function PaymentPage() {
+	const services = await getService();
 
-  const services = await getService();
-
-  return (
-    <section className="w-full p-5 flex flex-col gap-5 rounded-md bg-white text-black shadow-md md:p-10 xl:p-15">
-      <h1 className="border-b border-gray pb-5 text-lg font-bold">Más recientes</h1>
-      <ServiceList services={services} />
-    </section>
-  );
+	return (
+		<>
+			<ContainerService services={services} />
+		</>
+	);
 }
