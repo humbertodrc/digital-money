a1cbeb6effd3ede36d1e26062fe9a8dc64adee20
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-16 19:16:45 -0300

    📝 Add new SVG icons for various streaming services

---
+++ src/constants/service.ts
@@ -0,0 +1,55 @@
+interface ServiceImg {
+  src: string;
+  alt: string;
+}
+
+export const SERVICE = {
+  1: {
+    src: "/img/services/netflix.svg",
+    alt: "Logo de Netflix",
+  },
+  2: {
+    src: "/img/services/prime.svg",
+    alt: "Logo de Amazon Prime",
+  },
+  3: {
+    src: "/img/services/hbomax.svg",
+    alt: "Logo de HBO Max",
+  },
+  4: {
+    src: "/img/services/disney.svg",
+    alt: "Logo de Disney+",
+  },
+  5: {
+    src: "/img/services/paramount.svg",
+    alt: "Logo de Paramount+",
+  },
+  6: {
+    src: "/img/services/appletv.svg",
+    alt: "Logo de Apple TV+",
+  },
+  7: {
+    src: "/img/services/hulu.svg",
+    alt: "Logo de Hulu",
+  },
+  8: {
+    src: "/img/services/peacock.svg",
+    alt: "Logo de Peacock",
+  },
+  9: {
+    src: "/img/services/discovery.svg",
+    alt: "Logo de Discovery+",
+  },
+  10: {
+    src: "/img/services/crunchyroll.svg",
+    alt: "Logo de Crunchyroll",
+  },
+  11: {
+    src: "/img/services/funimation.svg",
+    alt: "Logo de Funimation",
+  },
+  12: {
+    src: "/img/services/mubi.svg",
+    alt: "Logo de Mubi",
+  },
+};
