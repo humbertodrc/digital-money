8335a28e91024e354f4f386536a823e29d3a0388
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-16 19:15:49 -0300

    🏗️ Refactor PaymentPage component to fetch and display services

--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -1,8 +1,14 @@
+import ServiceList from "@/components/dashboard/serviceList/ServiceList";
+import { getService } from "@/services/service";
 
-export default function PaymentPage() {
+export default async function PaymentPage() {
+
+  const services = await getService();
+
   return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
+    <section className="w-full p-5 flex flex-col gap-5 rounded-md bg-white text-black shadow-md md:p-10 xl:p-15">
+      <h1 className="border-b border-gray pb-5 text-lg font-bold">Más recientes</h1>
+      <ServiceList services={services} />
+    </section>
   );
 }
