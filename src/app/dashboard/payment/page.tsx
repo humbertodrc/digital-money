89c493f2760c651253f74b7d191d716cd13f4ee6
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-18 16:51:53 -0300

    🛠️ Refactor PaymentPage component to remove unnecessary code

--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -3,7 +3,6 @@ import { getService } from "@/services/service";
 
 export default async function PaymentPage() {
 	const services = await getService();
-
 	return (
 		<>
 			<ContainerService services={services} />
