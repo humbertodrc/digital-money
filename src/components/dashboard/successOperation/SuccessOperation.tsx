badc5f5a8178e277a2808b0938761ec93bee963e
Humberto Rivero <humbertorivero9@gmail.com>
2024-09-18 17:31:22 -0300

    🏗️ Refactor SuccessOperation component to include custom title and userId prop, and display selected card details

--- src/components/dashboard/successOperation/SuccessOperation.tsx
+++ src/components/dashboard/successOperation/SuccessOperation.tsx
@@ -1,35 +1,55 @@
 "use client";
 import Button from "@/components/common/button/Button";
 import CheckIcon from "@/components/common/Icons/CheckIcon";
+import {Card} from "@/interfaces/card";
+import {getCardById} from "@/services/cards";
 import {formatAmount} from "@/utils/formatAmount";
 import {formatDate} from "@/utils/formatDate";
+import {getCardType} from "@/utils/getCardType";
+import {maskCardNumber} from "@/utils/maskCardNumber";
 import {useRouter} from "next/navigation";
+import {useEffect, useState} from "react";
 import {useFormContext} from "react-hook-form";
 
 interface SuccessOperationProps {
 	origin?: string;
 	destiny: string;
+	title: string;
+	userId?: number;
+	isDeposit?: boolean;
 }
 
 export default function SuccessOperation({
 	origin,
 	destiny,
+	title,
+	userId,
+	isDeposit,
 }: SuccessOperationProps) {
+	const [selectedCard, setSelectedCard] = useState<Card>();
 	const {getValues} = useFormContext();
 	const date = new Date();
 	const router = useRouter();
+	const cardId = getValues("card_id");
+	const token = localStorage.getItem("acc_token") || "";
 
 	const handleBackToDashboard = () => {
 		router.push("/dashboard");
 	};
 
+	useEffect(() => {
+		if (!isDeposit) {
+			getCardById(userId, token, cardId).then((data) => {
+				setSelectedCard(data);
+			});
+		}
+	}, [cardId, isDeposit, token, userId]);
+
 	return (
 		<section className="flex flex-col gap-5">
 			<div className="w-full flex flex-col gap-3 items-center justify-center bg-primary py-4 rounded-lg">
 				<CheckIcon className="fill-black" />
-				<h2 className="text-xl text-black font-bold">
-					Ya cargamos el dinero en tu cuenta
-				</h2>
+				<h2 className="text-xl text-black font-bold">{title}</h2>
 			</div>
 			<div className="bg-black-primary p-5 xl:pt-9 xl:pl-16 xl:pb-11 rounded-lg">
 				<div className="flex flex-col gap-2 mb-8">
@@ -47,6 +67,15 @@ export default function SuccessOperation({
 						<>
 							<p>Brubank</p>
 							<p>{origin}</p>
+						</>
+					)}
+					{selectedCard && (
+						<>
+							<p>Tarjeta</p>
+							<div className="flex gap-1">
+								<p>{getCardType(String(selectedCard?.number_id))}</p>
+								<p>{maskCardNumber(String(selectedCard?.number_id))}</p>
+							</div>
 						</>
 					)}
 				</div>
