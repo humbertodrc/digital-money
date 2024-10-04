7d5f7200919b3b5e3b33382160f8d4d3aba80f10
Humberto Rivero <humbertorivero9@gmail.com>
2024-10-04 14:18:16 -0300

    🏗️ Refactor card schema validation to include additional checks for credit card number and name fields

--- src/schema/card.ts
+++ src/schema/card.ts
@@ -6,17 +6,19 @@ export const schemaCard = yup
 			.string()
 			.min(16, "Minimo 16 números")
 			.max(16, "Maximo 16 números")
-			.required("El campo es requerido"),
-		name: yup.string().required("El campo es requerido"),
+			.required("El campo es requerido")
+			.matches(/^[0-9]+$/, "Número inválido"),
+		name: yup.string().required("El campo es requerido").matches(/^[a-zA-Z ]+$/, "Nombre inválido"),
 		expiry: yup
 			.string()
 			.min(5, "Minimo 5 caracteres")
 			.max(5, "Maximo 5 caracteres")
 			.required()
-			.matches(/^(0[1-9]|1[0-2])\/\d{2}$/, "Invalid date"),
+			.matches(/^(0[1-9]|1[0-2])\/\d{2}$/, "Fecha inválida"),
     cvc: yup.string()
       .min(3, "Minimo 3 caracteres")
       .max(3, "Maximo 3 caracteres")
-      .required("El campo es requerido"),
+			.required("El campo es requerido")
+			.matches(/^[0-9]+$/, "Número inválido"),
 	})
 	.required();
